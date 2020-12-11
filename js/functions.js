cdde4777515c728db0cc0101297ad550d55c5a48
Daniel Porto <daporto@uninorte.edu.co>
2020-12-11 14:00:52 -0500

    Se creo menú seleccionar figura y se esconde al escoger una

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+function showboard(){
+    var x = document.getElementById("div-table");
+    var y = document.getElementById("menu");
+    x.style.display = "flex";
+    y.style.display = "none";
+}
