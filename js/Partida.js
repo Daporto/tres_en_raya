aecbef5627871141850169146a96585bdee9c37f
= <=>
2020-12-22 22:09:59 -0500

    Se valida correctamente cuando alguien gana a través de la función fijarposicion

--- js/Partida.js
+++ js/Partida.js
@@ -25,83 +25,66 @@ class Partida {
         }
     }
 
-    fijarposicion(state, f, c){
+    fijarposicion(state, f, c) {
         const posactual = this.board[f][c];
-        const cactual = posactual.counter;
         posactual.state = state;
-        if(f!=0 && state == this.board[f-1][c].state){
-            cactual[0]=this.board[f-1][c].counter[0]+1;
-            if(cactual[0]==this.filasganar){
-                return true;
-            }
+        const carround = [0, 0, 0, 0, 0, 0, 0, 0];
+        if (f != 0 && state == this.board[f - 1][c].state) {
+            carround[0] = this.board[f - 1][c].counter[0];
         }//Se suma 1 al contador de arriba
-        else{
-            cactual[0]=1;
-        }
-        if(f!=0 && c!=this.nfilas-1 && state == this.board[f-1][c+1].state){
-            cactual[1]=this.board[f-1][c+1].counter[1]+1;
-            if(cactual[1]==this.filasganar){
-                return true;
-            }
+        if (f != 0 && c != this.nfilas - 1 && state == this.board[f - 1][c + 1].state) {
+            carround[1] = this.board[f - 1][c + 1].counter[3];
         }//Se suma 1 al contador de arriba-derecha
-        else{
-            cactual[1]=1;
-        }
-        if(c!=this.nfilas-1 && state == this.board[f][c+1].state){
-            cactual[2]=this.board[f][c+1].counter[2]+1;
-            if(cactual[2]==this.filasganar){
-                return true;
-            }
+        if (c != this.nfilas - 1 && state == this.board[f][c + 1].state) {
+            carround[2] = this.board[f][c + 1].counter[1];
         }//Se suma 1 al contador de la derecha
-        else{
-            cactual[2]=1;
-        }
-        if(c!=this.nfilas-1 && f!=this.nfilas-1  && state == this.board[f+1][c+1].state){
-            cactual[3]=this.board[f+1][c+1].counter[3]+1;
-            if(cactual[3]==this.filasganar){
-                return true;
-            }
+        if (c != this.nfilas - 1 && f != this.nfilas - 1 && state == this.board[f + 1][c + 1].state) {
+            carround[3] = this.board[f + 1][c + 1].counter[2];
         }//Se suma 1 al contador de abajo-derecha
-        else{
-            cactual[3]=1;
-        }
-        if(f!=this.nfilas-1 && state == this.board[f+1][c].state){
-            cactual[4]=this.board[f+1][c].counter[4]+1;
-            if(cactual[4]==this.filasganar){
-                return true;
-            }
+        if (f != this.nfilas - 1 && state == this.board[f + 1][c].state) {
+            carround[4] = this.board[f + 1][c].counter[0];
         }//Se suma 1 al contador de abajo
-        else{
-            cactual[4]=1;
-        }
-        if(f!=this.nfilas-1 && c!=0 && state == this.board[f+1][c-1].state){
-            cactual[5]=this.board[f+1][c-1].counter[5]+1;
-            if(cactual[5]==this.filasganar){
-                return true;
-            }
+        if (f != this.nfilas - 1 && c != 0 && state == this.board[f + 1][c - 1].state) {
+            carround[5] = this.board[f + 1][c - 1].counter[3];
         }//Se suma 1 al contador de abajo-izquierda
-        else{
-            cactual[5]=1;
-        }
-        if(c!=0 && state == this.board[f][c-1].state){
-            cactual[6]=this.board[f][c-1].counter[6]+1;
-            if(cactual[6]==this.filasganar){
-                return true;
-            }
+        if (c != 0 && state == this.board[f][c - 1].state) {
+            carround[6] = this.board[f][c - 1].counter[1];
         }//Se suma 1 al contador de la izquierda
-        else{
-            cactual[6]=1;
-        }
-        if(c!=0 && f!=0 && state == this.board[f-1][c-1].state){
-            cactual[7]=this.board[f-1][c-1].counter[7]+1;
-            if(cactual[7]==this.filasganar){
-                return true;
-            }
+        if (c != 0 && f != 0 && state == this.board[f - 1][c - 1].state) {
+            carround[7] = this.board[f - 1][c - 1].counter[2];
         }//Se suma 1 al contador de la arriba-izquierda
-        else{
-            cactual[7]=1;
+        const newcounter = new Array(4);
+        newcounter[0] = carround[0] + carround[4] + 1;
+        newcounter[1] = carround[2] + carround[6] + 1;
+        newcounter[2] = carround[7] + carround[3] + 1;
+        newcounter[3] = carround[1] + carround[5] + 1;
+        //console.log(newcounter);
+        posactual.counter = newcounter.slice();
+        if (newcounter[0] >= this.filasganar || newcounter[1] >= this.filasganar || newcounter[2] >= this.filasganar || newcounter[3] >= this.filasganar) {
+            return true;
+        } else {
+            this.transmitir(0, -1, 0, f-1, c, newcounter[0], state);
+            this.transmitir(1, -1, 3, f-1, c+1, newcounter[3], state);
+            this.transmitir(1, 0, 1, f, c+1, newcounter[1], state);
+            this.transmitir(1, 1, 2, f+1, c+1, newcounter[2], state);
+            this.transmitir(0, 1, 1, f+1, c, newcounter[1], state);
+            this.transmitir(-1, 1, 3, f+1, c-1, newcounter[3], state);
+            this.transmitir(-1, 0, 1, f, c-1, newcounter[1], state);
+            this.transmitir(-1, -1, 2, f-1, c-1, newcounter[2], state);
         }
         return false;
+    }
+
+    transmitir(incx, incy, index, f, c, valor, state) {
+        if (f < this.nfilas && c < this.nfilas && f >= 0 && c >= 0) {
+            const pos = this.board[f][c];
+            if (pos.state == state) {
+                console.log("("+f+","+c+")");
+                pos.counter[index] = valor;
+                //console.log(pos.counter);
+                this.transmitir(incx, incy, index, f + incy, c + incx, valor, state);
+            }
+        }
     }
 
     createboard() {
@@ -117,8 +100,8 @@ class Partida {
             const fila = document.createElement("tr");
 
             for (var j = 0; j < this.nfilas; j++) {
-                const vector = [0,0,0,0,0,0,0,0];
-                const ficha = new PosicionTablero(i,j,0,vector);
+                const vector = [0, 0, 0, 0];
+                const ficha = new PosicionTablero(i, j, 0, vector);
                 this.board[i][j] = ficha;
                 const columna = document.createElement("th");
 
@@ -146,17 +129,18 @@ class Partida {
                 columna.onpointerleave = function () { this.innerHTML = ""; };
                 columna.onclick = function () {
                     const imagen = this.par.fichaimagenf.cloneNode(true);
-                    console.log("clase de la ficha: "+imagen.className);
+                    //console.log("clase de la ficha: " + imagen.className);
                     this.onpointerenter = NaN;
                     this.onpointerleave = NaN;
                     this.innerHTML = "";
                     this.onclick = NaN;
                     const f = this.closest('tr').rowIndex;
                     const c = this.cellIndex;
-                    const gano = this.par.fijarposicion(1,f,c);
-                    console.log("("+f+","+c+") = " + this.par.board[f][c].counter[5]);
+                    const gano = this.par.fijarposicion(1, f, c);
+                    //console.log("(" + f + "," + c + ") = " + this.par.board[f][c].counter[]);
+                    //console.log("(1,2): " + this.par.board[1][2].counter[3]);
                     this.appendChild(imagen);
-                    if(gano){
+                    if (gano) {
                         const divganar = document.getElementById("div-ganar");
                         const divtable = document.getElementById("div-table");
                         divtable.style.display = "none";
@@ -164,11 +148,11 @@ class Partida {
                     }
                 }
 
-            fila.appendChild(columna);
+                fila.appendChild(columna);
+            }
+            table.appendChild(fila);
         }
-        table.appendChild(fila);
+        divtable.appendChild(table);
     }
-    divtable.appendChild(table);
-}
 
 }
